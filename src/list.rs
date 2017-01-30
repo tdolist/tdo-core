61fddf5683d4d791498d68012df4175e1524425e
h4llow3En <mail@felixdoering.com>
2017-01-30 19:44:11 +0100

    add save and load function to Tdo

--- src/list.rs
+++ src/list.rs
@@ -1,4 +1,6 @@
 use todo::Todo;
+use std::fs::File;
+use storage::StorageError;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct TodoList {
@@ -23,8 +25,23 @@ pub struct Tdo {
 
 impl Tdo {
     pub fn new() -> Tdo {
-        Tdo {
-            lists: Vec::new(),
+        Tdo { lists: Vec::new() }
+    }
+
+    pub fn load(path: &str) -> Tdo {
+        let file = File::open(path).unwrap();
+        super::serde_json::from_reader(&file).unwrap()
+    }
+
+    pub fn save(path: &str, tdo: Tdo) -> Result<(), StorageError> {
+        match File::create(path) {
+            Ok(mut f) => {
+                let _ = super::serde_json::to_writer_pretty(&mut f, &tdo);
+                Ok(())
+            },
+            Err(_) => Err(StorageError::FileCorrupted)
         }
+
+
     }
 }
